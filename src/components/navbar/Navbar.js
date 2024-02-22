ba9ceba03de3889f047305341910318eef179d31
Federico Galuppi <locker9662@gmail.com>
2024-02-22 15:34:43 +0100

    quest2

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () =>
+{
+    return(
+        <>
+            <nav class="navbar navbar-expand-lg navbar-expand-sm bg-success mb-5">
+                <div className="container">
+                    <div class="navbar-nav">
+                        <Link class="btn btn-outline-light" to="/" >All Guilds</Link>
+                    </div>
+                    <div class="navbar-nav">
+                        <Link class="btn disabled text-light" to="/" >My Quest</Link>
+                    </div>
+                    <div class="navbar-nav">
+                        <Link class="btn btn-outline-light" to="/" >Login</Link>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
